cb5f82b61079fc8a5ec4f231fd0cd43a756dac1f
Ismael B B <ismbou9@alu.edu.gva.es>
2024-02-05 08:37:37 +0100

    descarda

---
+++ appversion1/target/classes/data.sql
@@ -0,0 +1,55 @@
+
+-- -----------------------------------------------------
+-- Table `usuarios`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `usuarios` ;
+
+CREATE  TABLE IF NOT EXISTS `usuarios` (
+  id BigInt  NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL ,
+  `nickname` VARCHAR(255) NOT NULL ,
+  `email` VARCHAR(255) NOT NULL ,
+  `password` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`),
+  CONSTRAINT usuario_uk_nickname UNIQUE KEY (`nickname`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `roles`
+-- -----------------------------------------------------
+
+DROP TABLE IF EXISTS `roles` ;
+
+CREATE  TABLE IF NOT EXISTS `roles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `usuarios_roles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `usuarios_roles` ;
+
+CREATE  TABLE IF NOT EXISTS `usuarios_roles` (
+  idUsuario BigInt  NOT NULL,  
+  idRol INT(11)  NOT NULL,
+  PRIMARY KEY (`idUsuario`,`idRol`),
+  CONSTRAINT `usuarios_roles_fk_usuarios`
+    FOREIGN KEY (`idUsuario` )
+    REFERENCES `usuarios` (`id` ),
+  CONSTRAINT `usuarios_roles_fk_roles`
+    FOREIGN KEY (`idRol` )
+    REFERENCES `roles` (`id` ) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+INSERT INTO `roles` (`id`, `nombre`) VALUES
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USER');
+
+-- -----------------------------------------------------
+-- FIN TABLAS NECESARIAS PARA IMPLEMENTAR LA SEGURIDAD ====LOGIN/REGISTRO
+-- -----------------------------------------------------
